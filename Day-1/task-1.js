61f2b5678e32da8ccddf92f787d28c4462bc1968
Ummay Nusra <nusraumme@gmail.com>
2026-06-16 18:07:42 +0600

    added practice: 1

--- Day-1/task-1.js
+++ Day-1/task-1.js
@@ -6,16 +6,23 @@
 
 const btn = document.getElementById('clickBtn');
 
-
 // btn.addEventListener('click', function(){
 //     btn.innerHTML = '';
 //     const pTag = document.createElement('p');
 //     pTag.textContent = 'Good Morning!';
 // });
 
+/*
+    *  Click এর পর button কে disable করে দাও
+    * Add করার আগে আগেরটা মুছে দাও
+*/
+
 btn.addEventListener('click', function(){
     const pTag = document.createElement('p');
     pTag.textContent = 'Good Morning!';
-    document.getElementById('pra').appendChild(pTag);
-    btn.style.display = 'none'
+    const text = document.getElementById('pra');
+    text.innerHTML = '';
+    text.appendChild(pTag);
+    // btn.disabled = true; 
 });
+
