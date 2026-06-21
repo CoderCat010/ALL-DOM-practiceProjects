b4e93fdab91d9fa9aa63c89536dce29242f2edb8
Ummay Nusra <nusraumme@gmail.com>
2026-06-22 01:25:19 +0600

    added practice-5

--- Day-1/task-5.js
+++ Day-1/task-5.js
@@ -1,1 +1,8 @@
 // Task 5: দুটো input এ দুটো number লিখে button click করলে যোগফল দেখাবে
+document.getElementById('clickBtn').addEventListener('click', () => {
+   const input1 = document.getElementById('input1');
+   const input2 = document.getElementById('input2');
+   const sum = parseInt(input1.value) + parseInt(input2.value);
+   const para = document.getElementById('pra');
+   para.textContent = `total sum: ${sum}`;
+})
