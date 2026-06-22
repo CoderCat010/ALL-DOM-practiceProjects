b27d71861d372b808f2496a03b2f96ff94453c2f
Ummay Nusra <nusraumme@gmail.com>
2026-06-23 05:05:49 +0600

    added task-3 all funtionality and final practice task

--- Day-2/task-3.js
+++ Day-2/task-3.js
@@ -1,3 +1,16 @@
 // Task 3: একটা list আছে — ["Apple", "Banana", "Mango", "Orange"] — এগুলো automatically page এ list হিসেবে দেখাবে।
+const list_container = document.getElementById('list-container');
 
 const arrayElm = ["Apple", "Banana", "Mango", "Orange"];
+
+// for(let elm of arrayElm){
+//     const list = document.createElement('li');
+//     list.textContent = elm;
+//     list_container.appendChild(list)
+// }
+
+arrayElm.forEach((elm) => {
+    const list = document.createElement('li');
+    list.textContent = elm;
+    list_container.appendChild(list);
+})
