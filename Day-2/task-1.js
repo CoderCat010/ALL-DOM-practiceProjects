2baf3ff244080a070fdbe6d44a726ef30c1f6a44
Ummay Nusra <nusraumme@gmail.com>
2026-06-22 07:55:02 +0600

    added practice-2

--- Day-2/task-1.js
+++ Day-2/task-1.js
@@ -1,8 +1,8 @@
 // Task 1: Button click করলে একটা counter বাড়বে। (0, 1, 2, 3...)
 let counter = 0;
+const ptag = document.getElementById('p');
 
 document.getElementById('ClickBtn').addEventListener('click', () => {
     counter++;
-    const ptag = document.getElementById('p');
     ptag.textContent = counter;
 })
