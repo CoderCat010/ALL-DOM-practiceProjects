e37ef771dc274a024a6b69a4996b75c9e745f177
Ummay Nusra <nusraumme@gmail.com>
2026-06-23 04:43:44 +0600

    added task-2 all funtionality and final practice task

--- Day-2/task-2.js
+++ Day-2/task-2.js
@@ -1,2 +1,13 @@
 // Task 2: একটা input আর button। Click করলে input এর value একটা list এ add হবে এবং নিচে দেখাবে।
-const inputBox = document.getElementById()
+const input_box = document.getElementById('input');
+const list_container = document.getElementById('list-container');
+
+function clickBtn(){
+    if(input_box.value === "") return;
+
+    // create ul list 
+    const list = document.createElement('li');
+    list.textContent = input_box.value;
+    input_box.value = "";
+    list_container.appendChild(list);
+}
