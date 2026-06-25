01f5958b6354b8674404505588519833780b34a0
Ummay Nusra <nusraumme@gmail.com>
2026-06-26 02:20:13 +0600

    added task-2 all elements

--- Day-3/task-2.js
+++ Day-3/task-2.js
@@ -1,4 +1,5 @@
 /* Task 2: Students এর একটা array of objects আছে — [{name: "Rahim", marks: 85}, {name: "Karim", marks: 92}
    * {name: "Jamal", marks: 78}] সবার নাম আর marks list হিসেবে দেখাবে। উপরের same array — button click করলে শুধু যাদের marks 80 এর উপরে তাদের দেখাবে। 
+
    * একটা input আর button। নাম আর marks লিখলে নতুন student add হবে list এ।
 */
