0a75ee39780b47574b87c27379f52898f32dc0a0
Ummay Nusra <nusraumme@gmail.com>
2026-06-27 04:36:02 +0600

    added task-2

--- Day-3/task-2.js
+++ Day-3/task-2.js
@@ -3,9 +3,14 @@
 
    * {name: "Jamal", marks: 78}] সবার নাম আর marks list হিসেবে দেখাবে। উপরের same array — button click করলে শুধু যাদের marks 80 এর উপরে তাদের দেখাবে। 
 */
-
 const listContainer = document.getElementById('list-container');
+const studentNames = document.getElementById('add-studentName');
+const studentMarks = document.getElementById('add-studentmarks');
+const lessThan = document.getElementById('less-than');
+const greaterThan = document.getElementById('greater-than');
 
+
+// data
 const arrOfObj = [
    {name: "Rahim", marks: 85}, 
    {name: "Karim", marks: 92},
@@ -13,22 +18,51 @@ const arrOfObj = [
 ];
 
 // loop through each one array object's data
-arrOfObj.forEach((eachOneData) => {
-   // create students list
-   const listItems = document.createElement('li');
-   listItems.classList.add(
-      "flex", "justify-between", "font-semibold", "text-md", "bg-fuchsia-100", "py-2", "px-4", "mb-5", "rounded-xl", "shadow-lg");
-   const span1 = document.createElement('span');
-   const span2 = document.createElement('span');
-   span1.textContent = `Name: ${eachOneData.name}`;
-   span2.textContent = `Marks: ${eachOneData.marks}`;
+function renderList(allData){
+   let filteredData = allData;
+   listContainer.innerHTML = '';
+   filteredData.forEach((eachOneData) => {
+      // create students list
+      const listItems = document.createElement('li');
+      listItems.classList.add(
+         "flex", "justify-between", "font-semibold", "text-md", "bg-fuchsia-100", "py-2", "px-5", "mb-5", "rounded-xl", "shadow-lg");
+
+      // create li span
+      const span1 = document.createElement('span');
+      const span2 = document.createElement('span');
+      const span3 = document.createElement('span');
+      // set object data in span tag
+      span1.textContent = `Name: ${eachOneData.name}`;
+      span2.textContent = '—';
+      span3.textContent = `Marks: ${eachOneData.marks}`;
+
+      // apend all the child to the page
+      listItems.appendChild(span1);
+      listItems.appendChild(span2);
+      listItems.appendChild(span3);
+      listContainer.appendChild(listItems)
+   });
+}
+renderList(arrOfObj);
+
+// add new students list when clicked add button
+document.getElementById('add-btn').addEventListener('click', () => {
+  let name = studentNames.value;
+  let marks = studentMarks.value;
+  if(name === "" || marks === "") return;
+
+  arrOfObj.push({name: name, marks: marks});
+  renderList(arrOfObj);
+
+   studentNames.value = '';
+   studentMarks.value = '';
+});
    
-   // apend all the child to the page
-   listItems.appendChild(span1);
-   listItems.appendChild(span2);
-   listContainer.appendChild(listItems)
-})
 
+greaterThan.addEventListener('click', () => {
+    renderList(arrOfObj.filter(s => s.marks > 80));
+});
 
-
-
+lessThan.addEventListener('click', () => {
+    renderList(arrOfObj.filter(s => s.marks < 80));
+});
