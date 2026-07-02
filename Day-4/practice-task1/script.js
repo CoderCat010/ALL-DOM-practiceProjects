084c62794594df5ae4b2a712e93bf478e0be4879
Ummay Nusra <nusraumme@gmail.com>
2026-07-02 08:45:01 +0600

    added all elements

---
+++ Day-4/practice-task1/script.js
@@ -0,0 +1,8 @@
+// all elements
+const addBtn = document.getElementById('add-btn');
+const noteBox = document.getElementById('type-note');
+const importentNotesCheckBox = document.getElementById('importent-notes-checkbox');
+const searchBox = document.getElementById('search-box');
+const importentNotes = document.getElementById('importent-notes');
+const notesCounter = document.getElementById('total-notes');
+const notesListContainer = document.getElementById('notes-list-container');
