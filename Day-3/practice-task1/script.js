84c1babd00f2de85970bcf22a5897139902e4515
Ummay Nusra <nusraumme@gmail.com>
2026-07-06 08:14:20 +0600

    added comparison functionality

--- Day-3/practice-task1/script.js
+++ Day-3/practice-task1/script.js
@@ -12,4 +12,23 @@ document.getElementById('all-candidates').addEventListener('click', (event) => {
     let voteCounterText = Number(voteCounterSpan.textContent);
     voteCounterText++;
     voteCounterSpan.textContent = voteCounterText;
+
+    // get max vote counter 
+    const allCounterSpan = document.querySelectorAll('.vote-counterBtn');
+    const allVoteCounterValues = [...allCounterSpan].map((allValues) => Number(allValues.textContent));
+    const crownWinner = Math.max(...allVoteCounterValues);
+
+    // compare each one card counter span values with max vote counter 
+    const allCandidates = document.querySelectorAll('.candidate-card');
+    allCandidates.forEach((candidateCard) => {
+        const cardCounterSpan = Number(candidateCard.querySelector('.vote-counterBtn').textContent);
+        const crownBadge = candidateCard.querySelector('.crown-winner');
+
+        // compare 
+        if(cardCounterSpan === crownWinner){
+            crownBadge.classList.remove('hidden');
+        }else{
+            crownBadge.classList.add('hidden');
+        }
+    })
 })
