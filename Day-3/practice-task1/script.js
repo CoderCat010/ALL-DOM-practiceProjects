d3e17546ef221c55e4dd0a9c9c52d0accec710eb
Ummay Nusra <nusraumme@gmail.com>
2026-07-06 07:55:04 +0600

    added vote counter buttons functionality

--- Day-3/practice-task1/script.js
+++ Day-3/practice-task1/script.js
@@ -1,35 +1,15 @@
-// get all candidates 
 document.getElementById('all-candidates').addEventListener('click', (event) => {
-    const selectedItems = event.target;
-
+    const selectedItem = event.target;
+    
     // if user clicked vote button or not
-    if(!selectedItems.classList.contains('vote-btn')) return;
-    
-    // clicked card 
-    const candidateCard = selectedItems.closest('.candidate-card');
-    
-    // counter button
-    const counterSpan = candidateCard.querySelector('.vote-counterBtn');
-    let currentVotes = Number(counterSpan.textContent);
-    currentVotes++;
-    counterSpan.textContent = currentVotes;
+    if(!selectedItem.classList.contains('vote-btn')) return;
 
-    // compare each one vote counts and get crown winner 
-    const allCounterValues = document.querySelectorAll('.vote-counterBtn');
-    const allVoteValues = [...allCounterValues].map((values) => Number(values.textContent));
-    const winner = Math.max(...allVoteValues);
-    
-    // winner 
-    const allCards = document.querySelectorAll('.candidate-card');
+    // get selected vote button's card
+    const selectedCard = selectedItem.closest('.candidate-card');
 
-    allCards.forEach((card) => {
-         const cardCounter = Number(card.querySelector('.vote-counterBtn').textContent);
-         const crown = card.querySelector('.crown-winner');
-
-         if(cardCounter === winner){
-            crown.classList.remove('hidden')
-         }else{
-            crown.classList.add('hidden')
-         }
-    })
+    // get vote counter button of selected card
+    const voteCounterSpan = selectedCard.querySelector('.vote-counterBtn');
+    let voteCounterText = Number(voteCounterSpan.textContent);
+    voteCounterText++;
+    voteCounterSpan.textContent = voteCounterText;
 })
