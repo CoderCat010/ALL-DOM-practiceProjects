309d7b58766aea05747295ed0eb62f7f51267d8a
Ummay Nusra <nusraumme@gmail.com>
2026-07-14 09:13:33 +0600

    added more functionality

--- Day-3/practice-task2/script.js
+++ Day-3/practice-task2/script.js
@@ -11,6 +11,15 @@ const trackers_container = document.getElementById('trackers-container');
 function trackerList_Render(each_one_data){
     trackers_container.innerHTML = '';
     each_one_data.forEach((data) => {
+        const isCompleted = data.daysCompleted === data.target;
+        let buttonText = 'Mark today done';
+        let disabledAttr = '';
+    
+        if(isCompleted){
+            buttonText = 'Completed 🎉';
+            disabledAttr = 'disabled';
+        }
+
         trackers_container.innerHTML += `
         <div data-id="${data.id}" class="habit-card flex justify-between items-center border bg-[#2C2C2A] p-3 rounded-[10px]">
             <!-- title -->
@@ -21,7 +30,11 @@ function trackerList_Render(each_one_data){
 
             <!-- progress button -->
             <div>
-                <button onclick="progress_btn(this)" class="py-1 px-2 shadow-sm border border-[#ffffff1a] rounded-md text-[#fff] font-medium text-[14px]">Mark today done</button>
+                <button 
+    onclick="progress_btn(this)" 
+    class="mark-today-done py-1 px-2 shadow-sm border border-[#ffffff1a] rounded-md text-[#fff] font-medium text-[14px]" 
+    ${disabledAttr}
+>${buttonText}</button>
             </div>
         </div>`
     })
@@ -32,6 +45,13 @@ trackerList_Render(habitsTracker);
 // progress button 
 function progress_btn(progress){
     const each_one_card = progress.closest('.habit-card');
-    console.log(each_one_card);
-    
+    const habitId = Number(each_one_card.dataset.id);
+
+    // find a single array object 
+    let habitObj = habitsTracker.find((habitsTracker_id) => habitsTracker_id.id === habitId);
+    if(habitObj.daysCompleted < habitObj.target){
+        habitObj.daysCompleted++;
+    }
+
+    trackerList_Render(habitsTracker);
 }
