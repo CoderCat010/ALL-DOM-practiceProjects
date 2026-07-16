4a47ff88b884571baf9100238b0d2dd7f952e665
Ummay Nusra <nusraumme@gmail.com>
2026-07-16 09:18:32 +0600

    updated more

--- Day-3/practice-task3/script.js
+++ Day-3/practice-task3/script.js
@@ -4,37 +4,25 @@ bookShop = [
    {id: 3, title: "1984", price: 300, bought: false},
 ];
 
-const dashboard = document.getElementById('dashboard');
+const bookItemsContainer = document.getElementById('bookItems-Container');
 
 // render 
 function renderItems(booksData){
+    bookItemsContainer.innerHTML = '';
     booksData.forEach((data) => {
-        dashboard.innerHTML = `
-        <!-- 
-        calculation
-          -->
-        <div class="flex justify-between text-black font-semibold" id="calculation">
-            <h2>Total:$ <span>1355</span></h2>
-            <h2>Spent:$ <span>450</span></h2>
-        </div>
+        bookItemsContainer.innerHTML += `
+        <div class="book-card flex justify-between items-center shadow-md bg-amber-200 p-3 rounded-[10px]">
+        <!-- title -->
+            <div>
+                <h2 class="text-[#000] font-semibold">${data.title}</h2>
+                <p class="text-[#000000c4] font-medium">$ ${data.price}</p>
+            </div>
 
-        <!-- 
-        book items container
-        -->
-        <div id="bookItems-Container">
-            <div class="habit-card flex justify-between items-center shadow-md bg-amber-200 p-3 rounded-[10px]">
-                <!-- title -->
-                <div>
-                    <h2 class="text-[#000] font-semibold">Atomic Habit</h2>
-                    <p class="text-[#000000c4] font-medium">$ 450</p>
-                </div>
-
-                <!-- progress button -->
-                <div>
-                    <button class="mark-today-done py-1 px-2 shadow-sm border border-[#0000001a] rounded-md text-[#000] font-medium text-[14px]">Buy</button>
-                </div>
+        <!-- progress button -->
+            <div>
+                <button class="mark-today-done py-1 px-2 shadow-sm border border-[#0000001a] rounded-md text-[#000] font-medium text-[14px]">Buy</button>
             </div>
         </div>`
-    })
+    });
 }
 renderItems(bookShop);
