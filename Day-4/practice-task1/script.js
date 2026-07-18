8d320dda24035df0dc10081d7a8ab61962993f6b
Ummay Nusra <nusraumme@gmail.com>
2026-07-18 12:13:01 +0600

    added rendering functionality

--- Day-4/practice-task1/script.js
+++ Day-4/practice-task1/script.js
@@ -3,6 +3,7 @@ const movies = [
    {id: 2, title: "The Dark Knight", genre: "Action", rating: 9.0, watched: false},
    {id: 3, title: "Interstellar", genre: "Sci-Fi", rating: 8.6, watched: false},
    {id: 4, title: "Parasite", genre: "Thriller", rating: 8.6, watched: false},
+   {id: 4, title: "The Great Wall", genre: "Fantasy", rating: 8.2, watched: false}
 ];
 
 // genre filter 
@@ -14,3 +15,25 @@ const watchedCounter = document.getElementById('watched-counter');
 
 // movie collections container 
 const moviesCollection = document.getElementById('all-movies-collection');
+
+
+// rendering 
+function renderingAllItems(moviesData){
+    moviesData.forEach(data => {
+        moviesCollection.innerHTML += `
+        <!-- movie cards -->
+            <div class="text-center bg-[#00ffdd2d] shadow-md rounded-xl w-[250px] py-5 space-y-5">
+                <!-- title -->
+                <h2 class="font-semibold text-2xl">${data.title}</h2>
+                <!-- genre & rating -->
+                <div class="flex justify-center gap-x-4">
+                    <h3>Genre: ${data.genre}</h3>
+                    <h3>Rating: ${data.rating}</h3>
+                </div>
+                <!-- watch button -->
+                <button class="bg-emerald-300 py-2 px-3 rounded-md">Tap To Watch</button>
+            </div>
+        `
+    });
+}
+renderingAllItems(movies);
