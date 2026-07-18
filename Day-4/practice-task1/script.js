05d958dee9d4e809945a2cab8e8dbf63a69cf8d1
Ummay Nusra <nusraumme@gmail.com>
2026-07-18 11:53:36 +0600

    added all elements

---
+++ Day-4/practice-task1/script.js
@@ -0,0 +1,6 @@
+const movies = [
+   {id: 1, title: "Inception", genre: "Sci-Fi", rating: 8.8, watched: false},
+   {id: 2, title: "The Dark Knight", genre: "Action", rating: 9.0, watched: false},
+   {id: 3, title: "Interstellar", genre: "Sci-Fi", rating: 8.6, watched: false},
+   {id: 4, title: "Parasite", genre: "Thriller", rating: 8.6, watched: false},
+];
