448850f0b03ef21ebd0ee4eb338838ee11a12f1d
Ummay Nusra <nusraumme@gmail.com>
2026-07-30 16:00:40 +0600

    added all elements

---
+++ Day-4 (Phase-2)/practice-task3/script.js
@@ -0,0 +1,10 @@
+// all element
+const notesContainer = document.getElementById('notes-container');
+
+
+// data
+let notes = [
+   {id: 1, subject: "Math", topic: "Algebra Basics", pinned: false},
+   {id: 2, subject: "Science", topic: "Photosynthesis", pinned: false},
+   {id: 3, subject: "History", topic: "World War II", pinned: false},
+];
