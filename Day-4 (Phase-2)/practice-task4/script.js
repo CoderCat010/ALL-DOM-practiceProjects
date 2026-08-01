f3cbed661d86fd7ecb6f90206adf07d237996558
Ummay Nusra <nusraumme@gmail.com>
2026-08-01 10:18:27 +0600

    added element & data

--- Day-4 (Phase-2)/practice-task4/script.js
+++ Day-4 (Phase-2)/practice-task4/script.js
@@ -8,3 +8,24 @@ let customers = [
    {id: 2, name: "Karim Enterprise", contact: "01822222222", note: "Follow up next week"},
    {id: 3, name: "Nabila Fashion", contact: "01933333333", note: "Wants price list"},
 ];
+
+// create elements & add styles, text content lastly append all the childs to their parent container.
+function elementsFactory(elements){
+    // create card's parent div -----> view mode
+    const primaryDiv = document.createElement('div');
+    const secondaryDiv = document.createElement('div');
+    const div1 = document.createElement('div');
+    const div1Title = document.createElement('h2');
+    const div1Description = document.createElement('p');
+    const div2 = document.createElement('div');
+    const div2Btn1 = document.createElement('button');
+    const div2Btn2 = document.createElement('button');
+    const viewModenote = document.createElement('p'); 
+    //-----> edit mode
+    const editModeBtn1 = document.createElement('button');
+    const editModeBtn2 = document.createElement('button');
+    const editModenote = document.createElement('input'); 
+
+    
+    
+}
