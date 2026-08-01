c003b298993763e87deabacf36e7102d7f746feb
Ummay Nusra <nusraumme@gmail.com>
2026-08-01 09:34:26 +0600

    added element & data

---
+++ Day-4 (Phase-2)/practice-task4/script.js
@@ -0,0 +1,10 @@
+// element
+const customersContainer = document.getElementById('customers-container');
+
+
+// data 
+let customers = [
+   {id: 1, name: "Rahim Traders", contact: "01711111111", note: "Interested in bulk order"},
+   {id: 2, name: "Karim Enterprise", contact: "01822222222", note: "Follow up next week"},
+   {id: 3, name: "Nabila Fashion", contact: "01933333333", note: "Wants price list"},
+];
