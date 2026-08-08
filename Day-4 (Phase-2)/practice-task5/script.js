49c9a4b2e83d490cd1d95dddaf374502c21ceab9
Ummay Nusra <nusraumme@gmail.com>
2026-08-08 23:22:13 +0600

    added final task

--- Day-4 (Phase-2)/practice-task5/script.js
+++ Day-4 (Phase-2)/practice-task5/script.js
@@ -27,11 +27,10 @@ function createBtns(btnElements){
     primaryBtn.classList.add('bg-[#20241F]',  'text-white', 'py-1.5', 'px-4', 'text-sm',  'font-medium', 'rounded-full');
     // add text
     primaryBtn.textContent = 'All';
-    // "All" button click
+     // "All" button click
     primaryBtn.addEventListener('click', () => {
         renderingCards(bookings);
     });
-
 
     // loop through each one btn 
    const statusAllBtn =  uniqueBtn.map((btn) => {
