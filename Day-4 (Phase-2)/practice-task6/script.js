8253e9117d78d9866536b7ae94a2766e3802c63d
Ummay Nusra <nusraumme@gmail.com>
2026-08-09 09:22:55 +0600

    added all elements & data

---
+++ Day-4 (Phase-2)/practice-task6/script.js
@@ -0,0 +1,11 @@
+// all elements
+const filterContainer = document.getElementById('filter-container');
+const ingredientsContainer = document.getElementById('ingredients-container');
+let uniqueBtns = [];
+
+// data 
+let ingredients = [
+   {id: 1, name: "Flour", quantity: 5, unit: "kg", minRequired: 2},
+   {id: 2, name: "Sugar", quantity: 1, unit: "kg", minRequired: 2},
+   {id: 3, name: "Eggs", quantity: 12, unit: "pcs", minRequired: 6},
+];
