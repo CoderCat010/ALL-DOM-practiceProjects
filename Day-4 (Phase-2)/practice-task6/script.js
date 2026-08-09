244484b68d3716c10cff38200d5eda07af0980cc
Ummay Nusra <nusraumme@gmail.com>
2026-08-09 09:29:20 +0600

    created all elements

--- Day-4 (Phase-2)/practice-task6/script.js
+++ Day-4 (Phase-2)/practice-task6/script.js
@@ -9,3 +9,21 @@ let ingredients = [
    {id: 2, name: "Sugar", quantity: 1, unit: "kg", minRequired: 2},
    {id: 3, name: "Eggs", quantity: 12, unit: "pcs", minRequired: 6},
 ];
+
+// create ingredients card
+function ingredCardFactory(cardsData){
+    // create parent card and inner elements
+    const card = document.createElement('div');
+    const topRow = document.createElement('div');
+    const infoDiv = document.createElement('div');
+    const title = document.createElement('h2');
+    const quantityText = document.createElement('p');
+    const deleteBtn = document.createElement('button');
+    const warningText = document.createElement('p');
+    const controlsRow = document.createElement('div');
+    const controlsBox = document.createElement('div');
+    const minusBtn = document.createElement('button');
+    const countSpan = document.createElement('span');
+    const plusBtn = document.createElement('button');
+
+}
