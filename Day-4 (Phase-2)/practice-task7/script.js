db2e2d9dc31b14f2cb9297ecd0c5ead65f9963d7
Ummay Nusra <nusraumme@gmail.com>
2026-08-15 23:21:44 +0600

    create card elements

--- Day-4 (Phase-2)/practice-task7/script.js
+++ Day-4 (Phase-2)/practice-task7/script.js
@@ -10,3 +10,21 @@ let tasks = [
    {id: 3, title: "Write Documentation", assignee: "Nabila", column: "todo", priority: "low"},
    {id: 4, title: "Setup Database", assignee: "Rafi", column: "done", priority: "medium"},
 ];
+
+// CREATE TASK BOARD CARD
+function createTaskCard(elements){
+    const { title, assignee, column, priority, id } = elements;
+    
+    // create note and inner elements
+    const note = document.createElement('div');
+    const pin = document.createElement('div');
+    const titleText = document.createElement('h3');
+    const assigneeText = document.createElement('p');
+    const bottomRow = document.createElement('div');
+    const priorityBadge = document.createElement('span');
+    const deleteBtn = document.createElement('button');
+    const moveBtn = document.createElement('button');
+    
+
+};
+
